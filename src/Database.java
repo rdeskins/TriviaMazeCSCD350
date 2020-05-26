eb3af12e039dd50d094efc5788bbf3992ce43e9c
Robin <rdeskins17@gmail.com>
2020-05-25 17:44:31 -0700

    Database class handles Exceptions

--- src/Database.java
+++ src/Database.java
@@ -1,3 +1,4 @@
+
 import java.sql.*;
 import java.util.ArrayList;
 
@@ -8,20 +9,35 @@ import java.util.ArrayList;
 public class Database {
     private String databaseName;
 
-    public Database(String fileName) throws SQLException {
+    public Database(String fileName) {
         this.databaseName = fileName;
-        Connection c = connect();
-	    c.close();
     }
 
-    public void initialize() throws SQLException {
-        Connection c = connect();
-        Statement stmt = c.createStatement();
-        this.createTables(c, stmt);
-        this.insertDefaultDifficulties(c, stmt);
-        this.insertDefaultQuestionTypes(c, stmt);
-        stmt.close();
-        c.close();
+    public boolean initialize() {
+        boolean success = false;
+        Connection c = null;
+        Statement stmt = null;
+        try {
+            c = connect();
+            stmt = c.createStatement();
+            this.createTables(c, stmt);
+            this.insertDefaultDifficulties(c, stmt);
+            this.insertDefaultQuestionTypes(c, stmt);
+            success = true;
+        }
+        catch (SQLException e) {
+            success = false;
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                disconnect(c, stmt);
+            }
+            catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return success;
     }
 
     private void createTables(Connection c, Statement stmt) throws SQLException {
@@ -68,122 +84,257 @@ public class Database {
         stmt.executeUpdate(sql);
     }
 
-    public boolean insertQuestion(int diffID, int typeID, Question q) throws SQLException{
-        Connection c = connect();
-        String sql = "INSERT INTO Questions (ID, difficulty_ID, type_ID, question_text, answer_text) " +
-            "VALUES (?, ?, ?, ?, ?);";
-        PreparedStatement stmt = c.prepareStatement(sql);
-        stmt.setInt(1, getQuestionTotal());
-        stmt.setInt(2, diffID);
-        stmt.setInt(3, typeID);
-        stmt.setString(4, q.getQuestion());
-        stmt.setString(5, q.getAnswer());
-        stmt.executeUpdate();
-        c.close();
-        stmt.close();
-        return true;
+    public boolean insertQuestion(int diffID, int typeID, Question q) {
+        boolean success = false;
+        Connection c = null;
+        PreparedStatement stmt = null;
+        try {
+            c = connect();
+            String sql = "INSERT INTO Questions (ID, difficulty_ID, type_ID, question_text, answer_text) " +
+                "VALUES (?, ?, ?, ?, ?);";
+            stmt = c.prepareStatement(sql);
+            stmt.setInt(1, getQuestionTotal());
+            stmt.setInt(2, diffID);
+            stmt.setInt(3, typeID);
+            stmt.setString(4, q.getQuestion());
+            stmt.setString(5, q.getAnswer());
+            stmt.executeUpdate();
+            success = true;
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                disconnect(c, stmt);
+            }
+            catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return success;
     }
 
-    public void deleteAllQuestions() throws SQLException {
-        Connection c = connect();
-        Statement stmt = c.createStatement();
-        String sql = "DELETE FROM Questions;";
-        stmt.executeUpdate(sql);
-        stmt.close();
-        c.close();
+    public boolean deleteAllQuestions() {
+        boolean success = false;
+        Connection c = null;
+        Statement stmt = null;
+        try {
+            c = connect();
+            stmt = c.createStatement();
+            String sql = "DELETE FROM Questions;";
+            stmt.executeUpdate(sql);
+            success = true;
+        }
+        catch (SQLException e) {
+            success = false;
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                disconnect(c, stmt);
+            }
+            catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return success;
     }
 
-    public Question getRandomQuestion() throws SQLException, IllegalArgumentException {
+    //Return null if error
+    public Question getRandomQuestion() throws IllegalArgumentException {
         if (getQuestionTotal() == 0) {
             throw new IllegalArgumentException();
         }
-        Connection c = connect();
-        Statement stmt = c.createStatement();
-        String sql = "SELECT question_text, answer_text FROM Questions ORDER BY RANDOM() LIMIT 1;";
-        ResultSet rs = stmt.executeQuery(sql);
-        Question result = new Question(rs.getString("question_text"), rs.getString("answer_text"));
-        rs.close();
-        stmt.close();
-        c.close();
+        Question result = null;
+        Connection c = null;
+        Statement stmt = null;
+        ResultSet rs = null;
+        try {
+            c = connect();
+            stmt = c.createStatement();
+            String sql = "SELECT question_text, answer_text FROM Questions ORDER BY RANDOM() LIMIT 1;";
+            rs = stmt.executeQuery(sql);
+            result = new Question(rs.getString("question_text"), rs.getString("answer_text"));
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                disconnect(c, stmt, rs);
+            }
+            catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
         return result;
     }
 
-    public Question getRandomQuestion(int difficultyID) throws SQLException, IllegalArgumentException {
+    public Question getRandomQuestion(int difficultyID) throws IllegalArgumentException {
         if (getQuestionTotal() == 0) {
             throw new IllegalArgumentException();
         }
-        Connection c = connect();
-        String sql = "SELECT question_text, answer_text FROM Questions " +
-            "WHERE difficulty_ID=? " +
-            "ORDER BY RANDOM() LIMIT 1;";
-        PreparedStatement stmt = c.prepareStatement(sql);
-        stmt.setInt(1, difficultyID);
-        ResultSet rs = stmt.executeQuery();
-        Question result = new Question(rs.getString("question_text"), rs.getString("answer_text"));
-        rs.close();
-        stmt.close();
-        c.close();
+        Question result = null;
+        Connection c = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        try {
+            c = connect();
+            String sql = "SELECT question_text, answer_text FROM Questions " +
+                "WHERE difficulty_ID=? " +
+                "ORDER BY RANDOM() LIMIT 1;";
+            stmt = c.prepareStatement(sql);
+            stmt.setInt(1, difficultyID);
+            rs = stmt.executeQuery();
+            result = new Question(rs.getString("question_text"), rs.getString("answer_text"));
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                disconnect(c, stmt, rs);
+            }
+            catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
         return result;
     }
 
-    public Question getQuestion(int qID) throws SQLException, IllegalArgumentException {
-        if (qID >= getQuestionTotal()) {
+    public Question getQuestion(int qID) throws IllegalArgumentException {
+        if (qID >= getQuestionTotal() || qID < 0) {
             throw new IllegalArgumentException();
         }
-        Connection c = connect();
-        String sql = "SELECT question_text, answer_text FROM Questions WHERE ID=?;";
-        PreparedStatement stmt = c.prepareStatement(sql);
-        stmt.setInt(1, qID);
-        ResultSet rs = stmt.executeQuery();
-        Question result = new Question(rs.getString("question_text"), rs.getString("answer_text"));
-        c.close();
-        stmt.close();
+        Question result = null;
+        Connection c = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        try {
+            c = connect();
+            String sql = "SELECT question_text, answer_text FROM Questions WHERE ID=?;";
+            stmt = c.prepareStatement(sql);
+            stmt.setInt(1, qID);
+            rs = stmt.executeQuery();
+            result = new Question(rs.getString("question_text"), rs.getString("answer_text"));
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                disconnect(c, stmt, rs);
+            }
+            catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
         return result;
     }
 
-    public int getQuestionTotal() throws SQLException {
-        Connection c = connect();
-        Statement stmt = c.createStatement();
-        String sql = "SELECT COUNT(*) FROM Questions;";
-        ResultSet rs = stmt.executeQuery(sql);
-        int total = rs.getInt("COUNT(*)");
-        rs.close();
-        c.close();
-        stmt.close();
+    //Returns -1 if error
+    public int getQuestionTotal() {
+        Connection c = null;
+        Statement stmt = null;
+        ResultSet rs = null;
+        int total = -1;
+        try {
+            c = connect();
+            stmt = c.createStatement();
+            String sql = "SELECT COUNT(*) FROM Questions;";
+            rs = stmt.executeQuery(sql);
+            total = rs.getInt("COUNT(*)");
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                disconnect(c, stmt, rs);
+            }
+            catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
         return total;
     }
 
-    public ArrayList<String> getQuestionTypes() throws SQLException {
+    public ArrayList<String> getQuestionTypes() {
         ArrayList<String> results = new ArrayList<String>();
-        Connection c = connect();
-        Statement stmt = c.createStatement();
-        String sql = "SELECT type_ID, type FROM Types;";
-        ResultSet rs = stmt.executeQuery(sql);
-        while (rs.next()) {
-            results.add(rs.getString("type_ID") + ". " + rs.getString("type"));
+        Connection c = null;
+        Statement stmt = null;
+        ResultSet rs = null;
+        try {
+            c = connect();
+            stmt = c.createStatement();
+            String sql = "SELECT type_ID, type FROM Types;";
+            rs = stmt.executeQuery(sql);
+            while (rs.next()) {
+                results.add(rs.getString("type_ID") + ". " + rs.getString("type"));
+            }
         }
-        rs.close();
-        c.close();
-        stmt.close();
+        catch (SQLException e) {
+            results = null;
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                disconnect(c, stmt, rs);
+            }
+            catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
         return results;
     }
 
-    public ArrayList<String> getDifficulties() throws SQLException {
+    public ArrayList<String> getDifficulties() {
         ArrayList<String> results = new ArrayList<String>();
-        Connection c = connect();
-        Statement stmt = c.createStatement();
-        String sql = "SELECT difficulty_ID, difficulty FROM Difficulties;";
-        ResultSet rs = stmt.executeQuery(sql);
-        while (rs.next()) {
-            results.add(rs.getString("difficulty_ID") + ". " + rs.getString("difficulty"));
+        Connection c = null;
+        Statement stmt = null;
+        ResultSet rs = null;
+        try {
+            c = connect();
+            stmt = c.createStatement();
+            String sql = "SELECT difficulty_ID, difficulty FROM Difficulties;";
+            rs = stmt.executeQuery(sql);
+            while (rs.next()) {
+                results.add(rs.getString("difficulty_ID") + ". " + rs.getString("difficulty"));
+            }
         }
-        rs.close();
-        c.close();
-        stmt.close();
+        catch (SQLException e) {
+            results = null;
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                disconnect(c, stmt, rs);
+            }
+            catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
         return results;
     }
 
     private Connection connect() throws SQLException {
         return DriverManager.getConnection("jdbc:sqlite:" + this.databaseName);
+    }
+
+    private void disconnect(Connection c, Statement stmt) throws SQLException {
+        if (stmt != null) {
+            stmt.close();
+        }
+        if (c != null) {
+            c.close();
+        }
+    }
+
+    private void disconnect(Connection c, Statement stmt, ResultSet rs) throws SQLException {
+        if (rs != null) {
+            rs.close();
+        }
+        disconnect(c, stmt);
     }
 }
